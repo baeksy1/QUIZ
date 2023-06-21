76459fc2c64769a6dbe11948b7fd060ddf3de37f
baeksy1 <133089793+baeksy1@users.noreply.github.com>
2023-06-21 19:18:56 +0900

    [Bronze V] Title: 문자와 문자열, Time: 224 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String []a= sc.next().split("");
+		int b = sc.nextInt();
+	System.out.println(a[b-1]);	
+	}
+	
+}
