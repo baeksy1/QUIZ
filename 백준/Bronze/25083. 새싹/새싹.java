464ceed693c13db67bdea723087a4141ef218841
baeksy1 <133089793+baeksy1@users.noreply.github.com>
2023-06-21 19:19:26 +0900

    [Bronze V] Title: 새싹, Time: 192 ms, Memory: 16736 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+
+		String s= "         ,r'\"7";
+		String s1= "r`-_   ,'  ,/";
+		String s2= " \\. \". L_r'";
+		String s3= "   `~\\/";
+		String s4= "      |";
+		String s5= "      |";
+		System.out.println(s+"\n"+s1+"\n"+s2+"\n"+s3+"\n"+s4+"\n"+s5+"\n");
+
+	}
+}
